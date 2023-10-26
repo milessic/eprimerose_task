0cc2480a859c8c4b6251a702c23e8119d6278c7d
milessic <53955481+milessic@users.noreply.github.com>
2023-10-26 12:29:08 +0200

    provided basic admin view

--- currency_exchange_database/api/admin.py
+++ currency_exchange_database/api/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from .models import Currencies
 
 
-class CurrenciesAdmin(admin.ModelAdmin):
-    search_fields = ('open_rate')
-    list_display = [field.name for field in Currencies._meta.get_fields()]
 
+class CurrenciesAdmin(admin.ModelAdmin):
+    search_fields = ('currency_pair',)
+    list_display = [field.name for field in Currencies._meta.get_fields()]
+    list_filter = ["base_currency", "quote_currency"]
 
 # Register your models here.
-admin.site.register(Currencies)
+admin.site.register(Currencies, CurrenciesAdmin)
