e7f75b7679a499f7f61294f671c58db3ac0415f4
milessic <53955481+milessic@users.noreply.github.com>
2023-10-26 09:25:26 +0200

    updated model to handle multiple currencies

--- currency_exchange_database/api/tests/test_models.py
+++ currency_exchange_database/api/tests/test_models.py
@@ -1,13 +1,19 @@
 from django.test import TestCase
-from ..models import EurUsdCurrencies
-
+from ..models import Currencies
+from datetime import date
 
 class TestModels(TestCase):
     def setUp(self):
-        self.currency_record = EurUsdCurrencies.objects.create(
-            open_rate=9.0598,
-            utc_timestamp="2023-10-23T09:19:28.703092Z"
+        self.currency_record = Currencies(
+            base_currency='EUR',
+            quote_currency='PLN',
+            open_rate=0.2232
         )
+        self.currency_record.save()
 
     def test_open_rate_can_be_read(self):
-        self.assertEquals(self.currency_record.open_rate, 9.0598)
+        self.assertEquals(self.currency_record.open_rate, 0.2232)
+        self.assertEquals(self.currency_record.currency_pair, "EURPLN")
+        currency_str = str(self.currency_record)
+        self.assertIn(date.today().strftime("%Y-%m-%d"), currency_str)
+
