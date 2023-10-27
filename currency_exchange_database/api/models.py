7c54107b16f9db461695edb56b1652bc9cb07d15
milessic <53955481+milessic@users.noreply.github.com>
2023-10-27 15:34:12 +0200

    cread model currencies and modified solution to properly show the values

--- currency_exchange_database/api/models.py
+++ currency_exchange_database/api/models.py
@@ -2,19 +2,23 @@ from django.db import models
 
 
 class Currencies(models.Model):
-    base_currency = models.CharField(max_length=3)
-    quote_currency = models.CharField(max_length=3)
+    currency_code = models.CharField(max_length=3, unique=True)
+    def __str__(self):
+        return self.currency_code
+
+
+class CurrencyRates(models.Model):
+    base_currency = models.ForeignKey(Currencies, on_delete=models.CASCADE, related_name="base")
+    quote_currency = models.ForeignKey(Currencies, on_delete=models.CASCADE, related_name="quote")
     open_rate = models.DecimalField(max_digits=10, decimal_places=4)
     utc_timestamp = models.DateTimeField(auto_now_add=True)
-    currency_pair = models.CharField(max_length=6)
 
     def __str__(self):
-        return f'{self.currency_pair} - {str(self.utc_timestamp)[:19]}'
+        return f'{self.base_currency}{self.quote_currency} - {str(self.utc_timestamp)[:19]}'
 
     def save(self, *args, **kwargs):
         # overriding save method to
-        self.base_currency = self.base_currency.upper()
-        self.quote_currency = self.quote_currency.upper()
-        self.currency_pair = f"{self.base_currency}{self.quote_currency}"
-        super(Currencies, self).save(*args, **kwargs)
+        # self.base_currency = self.base_currency.upper()
+        # self.quote_currency = self.quote_currency.upper()
+        super(CurrencyRates, self).save(*args, **kwargs)
 
--- currency_exchange_database/api/serializers.py
+++ currency_exchange_database/api/serializers.py
@@ -1,15 +1,23 @@
 from rest_framework import serializers
-from .models import Currencies
+from .models import CurrencyRates, Currencies
 from django.forms import ValidationError
 
 
-class CurrenciesSerializer(serializers.ModelSerializer):
+class CurrencySerializer(serializers.ModelSerializer):
     class Meta:
         model = Currencies
-        fields = '__all__'
+        fields = ('currency_code',)
+
+
+class CurrencyRatesSerializer(serializers.ModelSerializer):
+    base_currency = CurrencySerializer().fields['currency_code']
+    quote_currency = CurrencySerializer().fields['currency_code']
+
+    class Meta:
+        model = CurrencyRates
+        fields = "__all__"
 
     def validate(self, data):
         if len(data['base_currency']) != 3 or (data['quote_currency']) != 3 :
             raise ValidationError("Each currency code has to be 3 digits code.")
         return value
-
