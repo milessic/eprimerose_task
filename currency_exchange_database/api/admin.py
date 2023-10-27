7de2c24d029e76a0c48ee41cd3859669ef9a73b3
milessic <53955481+milessic@users.noreply.github.com>
2023-10-27 15:34:30 +0200

    added admin view for Currencies model

--- currency_exchange_database/api/admin.py
+++ currency_exchange_database/api/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
-from .models import Currencies
+from .models import CurrencyRates, Currencies
 from rangefilter.filters import DateRangeFilterBuilder
 
 
 class CurrenciesAdmin(admin.ModelAdmin):
+    search_fields = ('currency_code',)
+    list_display = ('currency_code', )
+
+
+class CurrencyRatesAdmin(admin.ModelAdmin):
     # basic admin interface with search field for currency_pair field, list display for all fields
     #   and filters for utc_timestamp (3rd party app django-rangefilter, built in filters for base_currency and quote_currency
-    search_fields = ('currency_pair',)
-    list_display = [field.name for field in Currencies._meta.get_fields()]
+    #   search fields searches for currency_code attirbute of base_currency or quote_currency
+    search_fields = ('base_currency__currency_code', 'quote_currency__currency_code')
+    list_display = [field.name for field in CurrencyRates._meta.get_fields()]
     list_filter = [("utc_timestamp", DateRangeFilterBuilder()), "base_currency", "quote_currency"]
 
+
 # Register your models here.
+admin.site.register(CurrencyRates, CurrencyRatesAdmin)
 admin.site.register(Currencies, CurrenciesAdmin)
